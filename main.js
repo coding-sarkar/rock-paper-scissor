0816152798a30a4d06097a59f0e2c7c9d2ada84c
Souvik Dey Sarkar <sdeys2111@gmail.com>
2022-04-18 00:41:02 +0530

    Add function to generate "Rock", "Paper", "Scissor" randomly

---
+++ main.js
@@ -0,0 +1,10 @@
+const computerPlay = function() {
+    let computerOption = Math.floor(Math.random() * 3) + 1;
+
+    if (computerOption === 1)
+        return "Rock";
+    else if (computerOption === 2)
+        return "Paper";
+    else
+        return "Scissor";
+}
