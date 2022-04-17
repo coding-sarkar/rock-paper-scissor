a71b9226fe9fe149f1faf8b9f6dbbc2411072db4
Souvik Dey Sarkar <sdeys2111@gmail.com>
2022-04-18 01:22:49 +0530

    Add playRound function to return result string

--- main.js
+++ main.js
@@ -8,3 +8,36 @@ function computerPlay() {
     else
         return "Scissor";
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerOpt = playerSelection.toLowerCase();
+    let compOpt = computerSelection.toLowerCase();
+
+    if (playerOpt === compOpt) {
+        return `Tie! ${playerSelection} can't beat ${computerSelection}`;
+    }
+
+    if (playerOpt === "rock") {
+        if (compOpt === "paper") {
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        } else {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    }
+
+    if (playerOpt === "paper") {
+        if (compOpt === "scissor") {
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        } else {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    }
+
+    if (playerOpt === "scissor") {
+        if (compOpt === "rock") {
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        } else {
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    }
+}
