a81738c69769a5726deb83fc78416825e41b9d9e
Souvik Dey Sarkar <sdeys2111@gmail.com>
2022-04-18 02:23:39 +0530

    Add game() to run five rounds

--- main.js
+++ main.js
@@ -2,46 +2,71 @@ function computerPlay() {
     let computerOption = Math.floor(Math.random() * 3) + 1;
 
     if (computerOption === 1)
-        return "Rock";
+        return "rock";
     else if (computerOption === 2)
-        return "Paper";
+        return "paper";
     else
-        return "Scissor";
+        return "scissor";
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerOpt = playerSelection.toLowerCase();
-    let compOpt = computerSelection.toLowerCase();
-
-    if (playerOpt === compOpt) {
-        return `Tie! ${playerSelection} can't beat ${computerSelection}`;
+    // if player loses it returns 0 otherwise 1
+    // if their is a tie just returns 
+    
+    if (playerSelection === computerSelection) {
+        return;
     }
 
-    if (playerOpt === "rock") {
-        if (compOpt === "paper") {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return 0;
         } else {
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+            return 1;
         }
     }
 
-    if (playerOpt === "paper") {
-        if (compOpt === "scissor") {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    if (playerSelection === "paper") {
+        if (computerSelection === "scissor") {
+            return 0;
         } else {
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+            return 1;
         }
     }
 
-    if (playerOpt === "scissor") {
-        if (compOpt === "rock") {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    if (playerSelection === "scissor") {
+        if (computerSelection === "rock") {
+            return 0;
         } else {
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+            return 1;
         }
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerSelection, computerSelection;
+    let player = 0;
+    let computer = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Enter [Rock/Paper/Scissor]");
+        computerSelection = computerPlay();
+        
+        let result = playRound(playerSelection.toLowerCase(), computerSelection);
+        
+        if (result === 1) {
+            player++;
+        } else if (result === 0){
+            computer++;
+        }
+    }
+
+    if (player > computer) {
+        console.log(`You Win! Player = ${player} | Computer = ${computer}`);
+    } else if (player < computer) {
+        console.log(`You Lose! Player = ${player} | Computer = ${computer}`);
+    } else {
+        console.log(`Tie! Player = ${player} | Computer = ${computer}`);
+    }
+}
+
+game();
